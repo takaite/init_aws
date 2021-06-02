6b739474565a319bd82cab6c1968e3b0a8b62bb2
Takaite Takehara <Takaite@brasilafora.com.br>
2021-06-02 12:04:45 -0300

    ohmyzsh.sh

--- install_ohmyzsh.sh
+++ install_ohmyzsh.sh
@@ -1,9 +1,9 @@
 echo "You need to set a password"
 
-sudo passwd
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo passwd $USER
 
 mv /usr/init_aws/zshrc ~/.zshrc
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
 
