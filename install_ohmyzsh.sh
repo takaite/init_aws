06ef2a7415b087425248e5e1f7a53a3914536911
Takaite Takehara <Takaite@brasilafora.com.br>
2021-06-02 14:55:07 -0300

    ohmyzsh.sh

--- install_ohmyzsh.sh
+++ install_ohmyzsh.sh
@@ -3,9 +3,9 @@ echo "You need to set a password"
 sudo passwd $USER
 
 cp /usr/init_aws/zshrc ~/.zshrc
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh --keep-zshrc)"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --keep-zshrc"
 
-echo "PATH=$PATH:/usr/init_aws" >> ~/.zshrc
+echo "PATH=\$PATH:/usr/init_aws" >> ~/.zshrc
 
 
 
