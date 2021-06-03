41853841d97fa09b00116fe0f9dff9ae02e87841
Takaite Takehara <Takaite@brasilafora.com.br>
2021-06-03 11:00:26 -0300

    nvim

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 
-add-apt-repository -y ppa:neovim-ppa/stable
-apt update
-apt install -y neovim
+sudo dd-apt-repository -y ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install -y neovim
 
 mkdir ~/.config
 mkdir ~/.config/nvim
 
 cp /usr/init_aws/init.vim ~/.config/nvim
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+nvim
 
