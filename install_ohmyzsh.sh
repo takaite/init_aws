2cc10ec7f0bc2dcad906d960995aceeb09e962c2
Takaite Takehara <Takaite@brasilafora.com.br>
2021-06-04 16:32:07 -0300

    Inserting nvm on ohmyzsh

--- install_ohmyzsh.sh
+++ install_ohmyzsh.sh
@@ -2,6 +2,7 @@ echo "You need to set a password"
 
 sudo passwd $USER
 
+git clone https://github.com/lukechilds/zsh-nvm ~/.oh-my-zsh/custom/plugins/zsh-nvm
 cp /usr/init_aws/zshrc ~/.zshrc
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --keep-zshrc"
 
